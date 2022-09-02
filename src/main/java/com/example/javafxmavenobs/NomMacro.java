f88992ab1c310e7689616dcfae185d9fd5bd5184
William Blot <william.blot@etu.univ-nantes.fr>
2022-09-02 12:31:25 +0200

    Modif liste

--- src/main/java/com/example/javafxmavenobs/NomMacro.java
+++ src/main/java/com/example/javafxmavenobs/NomMacro.java
@@ -4,9 +4,7 @@ public enum NomMacro {
     Aucune,
     T5,
     A2,
-    A9,
-
-    Test;
+    A9
 
     public static NomMacro getMacroByName(String name) {
         for (NomMacro macro : NomMacro.values()) {
